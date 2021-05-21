1dc98db90b47bf3301cfcf7f1a6bcfcb2553db4a
laiba taha <77200349+Laibataha08@users.noreply.github.com>
2021-05-21 10:28:07 +0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        Game game;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+       
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            game.update();  
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            game = Game.getInstance();
+            GameObject p = new GameObject(pictureBox1, new Moveupleft(),2, Type.player, label1);
+
+            GameObject redGhost = new GameObject(pictureBox2 , new MoveLeft(), 2 , Type.ghost , label2);
+
+            GameObject pinkGhost = new GameObject(pictureBox3, new MoveRight(), 2, Type.ghost , label2);
+
+            GameObject yellowGhost = new GameObject(pictureBox4 , new Falling(), 3 , Type.ghost , label2);
+
+            game.AddObjects(p);
+            game.AddObjects(redGhost);
+            game.AddObjects(pinkGhost);
+            game.AddObjects(yellowGhost);
+            game.getcount();
+
+        }
+
+      
+    }
+}
---
+++ GameObject.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+
+namespace WindowsFormsApp4
+{
+    class GameObject
+    {
+        IMovement move;
+
+        PictureBox pictureBox;
+        public int speed;
+        IMovement movement;
+        Type type;
+        Label label;
+        public static int player = 0;
+        public static int ghost ;
+
+
+        public GameObject(PictureBox pictureBox , IMovement movement , int speed, Type type, Label label)
+        {
+            this.pictureBox = pictureBox;
+            this.movement = movement;
+            this.speed = speed;
+            this.type = type;
+            this.label = label;
+        }
+
+        public void updateposition()
+        {
+            move = movement;
+            move.typeofmovement(speed, pictureBox);    
+        }
+
+
+        public void getcount()
+        {
+            if (Type.player == type)
+            {
+                player++;
+                label.Text = "player  : " + player ;
+            }
+
+
+            if (Type.ghost == type )
+            {
+                ghost++;
+                label.Text = "ghost : " + ghost;
+            }
+        }
+       
+    }
+
+    enum Type
+    {
+        player,
+        ghost,
+    }
+}
